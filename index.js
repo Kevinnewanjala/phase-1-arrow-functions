bdafa369b3e088f8d40355e05d7ac95d873bd51a
Kevin Wanjala <wanjalahkevin97@gmail.com>
2024-03-20 13:14:24 +0300

    Declared functions using arrow syntax

---
+++ index.js
@@ -0,0 +1,7 @@
+const square = (parameter1) => parameter1 * parameter1;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+
+const divide = (parameter1, parameter2) => parameter1 / parameter2;
+  parameter1 = 2000
+  parameter2 = 100
